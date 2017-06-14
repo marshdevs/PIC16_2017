1e8962f0c3b30dba06780e6a0668ddc833485cb9
Marshall Briggs <marshallbb@ucla.edu>
2017-06-14 13:58:49 -0700

    Create hw5.py

---
+++ Homework_05/hw5.py
@@ -0,0 +1,23 @@
+# Homework 5
+# Filename: hw5.py
+# Author: Marshall Briggs
+"""
+Challenge 1 corrcoeff(x,y)
+Write a function that two vectors of length n as input (representing two variables at n observations),
+and outputs the correlation coefficient between them.
+. . . . . . . . .
+Challenge 2 degree(N) and strength(N)
+Write a function that takes a (possibly weighted) network as input, and outputs two vectors, one of
+which gives the degree or strength (respectively) of each node, and one which ranks the nodes by
+degree/strength, highest to lowest.
+. . . . . . . . .
+Challenge 3 pagerank(N)
+Write a function that uses linear algebra from numpy that takes a (possibly weighted, directed network)
+as input, and outputs two vectors, one that gives the score for each node, and one vector that
+ranks the nodes from highest pagerank score to lowest.
+. . . . . . . . .
+Challenge 4 similarity(T)
+Write a function that takes a set of tweets (given as a list of one string for each tweet), and outputs a
+network where the edges represent the similarity between pairs of tweets, based on common words.
+Think about different ways to compute this score.
+"""
